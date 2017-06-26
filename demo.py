a0bbb9a61636e02cfe92adf919d7496a06801ccc
Scofieldsu <931180361@qq.com>
2017-06-26 15:00:53 +0800

    接口请求地址可自定义

--- demo.py
+++ demo.py
@@ -1,28 +1,25 @@
 # encoding: utf-8
 
-from flask import json
 from flaskapi.api import api_add
 
 
 @api_add
-def test_api2(param_dict, param_int, param_str, param_list):
+def login_test(name, pwd):
     """
-    :description  测试接口2
-    :param param_dict: dict:字典参数
-    :param param_int: int:整型
-    :param param_str: str:无默认值
-    :param param_list: list:可以省略[]
-    :return: code or message
+    :description test登录接口
+    :param name: str
+    :param pwd: str
+    :return: test登录信息
     """
-
-    data1 = json.loads(param_dict)
-    data2 = param_int
-    data3 = param_str
-    data4 = param_list
-    result = {
-        "param_dict": data1,
-        "param_int": data2,
-        "param_str": data3,
-        "param_list": data4
-    }
+    result = {"msg": "login success", "code": 200}
     return result
+
+
+@api_add
+def logout_test(name):
+    """
+    :description test退出接口
+    :param name: str
+    :return: test退出信息success or error
+    """
+    return "logout success"

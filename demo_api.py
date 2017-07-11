6c3454c927885ac625960f619a6134981b3b504c
Scofieldsu <931180361@qq.com>
2017-07-11 16:06:41 +0800

    分别用flaskapi和apidata实现返回接口信息

--- demo_api.py
+++ demo_api.py
@@ -5,23 +5,23 @@ from apidata import api_data
 
 
 @api_add
-def login_test(name, pwd):
+def login(name, pwd):
     """
-    :description test登录接口
+    :description 登录接口
     :param name: str
     :param pwd: str
-    :return: test登录信息
+    :return: 登录信息
     """
     result = {"msg": "login success", "code": 200}
     return result
 
 
 @api_add
-def logout_test(name):
+def logout(name):
     """
-    :description test退出接口
+    :description 退出接口
     :param name: str
-    :return: test退出信息success or error
+    :return: 退出信息success or error
     """
     return "logout success"
 
--- flaskapi/common.py
+++ flaskapi/common.py
@@ -5,6 +5,7 @@ from flaskapi.utils.api_tools import compose_api_info
 from apidata.common import get_all_api_data
 
 
+# 调用api-data获取接口信息
 @api_add
 def get_all_api(*args, **kwargs):
     """
@@ -13,22 +14,27 @@ def get_all_api(*args, **kwargs):
     :param kwargs:str
     :return: 所有接口信息
     """
-    # 从method_map获取所有接口方法，重组数格式
-    # _name = sys._getframe().f_code.co_name
-    # api_dict = api.dispatcher.method_map
-    # api_name_list = api_dict.keys()
-    # result = dict()
-    # for i in api_name_list:
-    #     item = dict()
-    #     item = compose_api_info(i, api_dict)
-    #     result[i] = item
-    # dirs = os.path.abspath('.')
-    result = get_all_api_data( decor_list=['api_add','api_data'])
+    result = get_all_api_data(file_type='py', decor_list=['api_add', 'api_data'])
+    result.pop("get_all_api_flask")
     result.pop("hello")
     return result
 
 
-# get_all_api返回的数据格式
+# 调用flask-api获取接口信息
+@api_add
+def get_all_api_flask(*args, **kwargs):
+    _name = sys._getframe().f_code.co_name
+    api_dict = api.dispatcher.method_map
+    api_name_list = api_dict.keys()
+    result = dict()
+    for i in api_name_list:
+        item = compose_api_info(i, api_dict)
+        result[i] = item
+    result.pop(_name)
+    result.pop("get_all_api")
+    return result
+
+# API-DATA协议  返回的数据格式
 def get_all_api_temp(*args, **kwargs):
     result = {'login': {'name': 'login', 'description': '登录接口', 'return': '返回信息', 'param_explain': {},  'params': {'login_name': 'str', "password": "str"}},
               'logout': {'name': 'logout', 'description': '退出', 'return': '返回信息',  'param_explain': {}, 'params': {"name": "str", "pwd": "str"}}}
